25f5fa29cee6ed866dae08e854adb3c3f02f26a9
xx-stephen-lang <venfocus@foxmail.com>
2025-03-04 14:30:16 +0800

    Add files via upload

---
+++ 锋鸣智能测试系统/script.js
@@ -0,0 +1,300 @@
+{\rtf1\ansi\ansicpg936\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 const model = \{\
+    dimensions: [\
+        \{ \
+            name: "\uc0\u35777 \u25454 \u20805 \u20998 \u24615 ", \
+            weightLegal: 18, \
+            weightPR: 5,\
+            criteria: [\
+                "\uc0\u20165 \u26377 \u21333 \u26041 \u38472 \u36848 ",\
+                "\uc0\u30005 \u23376 \u37038 \u20214 /\u32842 \u22825 \u35760 \u24405 \u31561 \u30005 \u23376 \u35777 \u25454 ",\
+                "\uc0\u20070 \u38754 \u21512 \u21516 +\u38134 \u34892 \u27969 \u27700 \u31561 \u30452 \u25509 \u35777 \u25454 ",\
+                "\uc0\u32463 \u20844 \u35777 \u30340 \u20070 \u38754 \u35777 \u25454 ",\
+                "\uc0\u21496 \u27861 \u37492 \u23450 \u25253 \u21578 +\u23436 \u25972 \u35777 \u25454 \u38142 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u27861 \u24459 \u20381 \u25454 ", \
+            weightLegal: 18, \
+            weightPR: 2,\
+            criteria: [\
+                "\uc0\u27861 \u24459 \u31354 \u30333 \u39046 \u22495 ",\
+                "\uc0\u38656 \u21496 \u27861 \u35299 \u37322 \u34917 \u27491 ",\
+                "\uc0\u23384 \u22312 \u20105 \u35758 \u21028 \u20363 ",\
+                "\uc0\u26126 \u30830 \u27861 \u35268 \u20294 \u38656 \u22797 \u26434 \u20030 \u35777 ",\
+                "\uc0\u21487 \u30452 \u25509 \u36866 \u29992 \u20855 \u20307 \u27861 \u26465 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u26102 \u38388 \u32039 \u36843 \u24615 ", \
+            weightLegal: 10, \
+            weightPR: 18,\
+            criteria: [\
+                "\uc0\u26080 \u26102 \u38480 \u35201 \u27714 ",\
+                "3-6\uc0\u20010 \u26376 \u21608 \u26399 ",\
+                "1-3\uc0\u20010 \u26376 \u21608 \u26399 ",\
+                "15-30\uc0\u26085 \u20851 \u38190 \u26399 ",\
+                "7\uc0\u26085 \u20869 \u32039 \u24613 \u24773 \u20917 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u21512 \u20316 \u26041 \u24577 \u24230 ", \
+            weightLegal: 15, \
+            weightPR: 5,\
+            criteria: [\
+                "\uc0\u39640 \u24230 \u37197 \u21512 ",\
+                "\uc0\u19968 \u33324 \u37197 \u21512 ",\
+                "\uc0\u24577 \u24230 \u27169 \u31946 ",\
+                "\uc0\u28040 \u26497 \u23545 \u25239 ",\
+                "\uc0\u23436 \u20840 \u25298 \u32477 \u21512 \u20316 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u26696 \u20214 \u26159 \u21542 \u28041 \u21450 \u20844 \u20849 \u21033 \u30410 ", \
+            weightLegal: 3, \
+            weightPR: 12,\
+            criteria: [\
+                "\uc0\u32431 \u21830 \u19994 \u32416 \u32439 \u65292 \u26080 \u20844 \u20849 \u21033 \u30410 ",\
+                "\uc0\u28041 \u21450 \u28040 \u36153 \u32773 \u26435 \u30410 \u65292 \u20294 \u26410 \u34987 \u20851 \u27880 ",\
+                "\uc0\u28041 \u21450 \u21171 \u24037 \u12289 \u29615 \u20445 \u12289 \u31246 \u21153 \u31561 \u31038 \u20250 \u35758 \u39064 ",\
+                "\uc0\u28041 \u21450 \u22269 \u23478 \u25919 \u31574 \u35843 \u25972 \u25110 \u37325 \u22823 \u34892 \u19994 \u21464 \u21160 ",\
+                "\uc0\u28041 \u21450 \u24191 \u27867 \u27665 \u29983 \u38382 \u39064 \u65292 \u24341 \u21457 \u20840 \u27665 \u20851 \u27880 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u26696 \u20214 \u26159 \u21542 \u28041 \u21450 \u30693 \u21517 \u20225 \u19994 \u25110 \u20844 \u20247 \u20154 \u29289 ", \
+            weightLegal: 3, \
+            weightPR: 12,\
+            criteria: [\
+                "\uc0\u26080 \u30693 \u21517 \u20225 \u19994 \u25110 \u20844 \u20247 \u20154 \u29289 \u28041 \u21450 ",\
+                "\uc0\u28041 \u21450 \u34892 \u19994 \u20108 \u32447 \u20225 \u19994 \u25110 \u21306 \u22495 \u30693 \u21517 \u20154 \u29289 ",\
+                "\uc0\u28041 \u21450 \u34892 \u19994 \u22836 \u37096 \u20225 \u19994 \u12289 \u20844 \u20247 \u20154 \u29289 ",\
+                "\uc0\u28041 \u26696 \u20027 \u20307 \u20026 \u22269 \u20225 \u12289 \u19978 \u24066 \u20844 \u21496 \u12289 \u22269 \u38469 \u26426 \u26500 ",\
+                "\uc0\u26696 \u20214 \u28041 \u21450 \u25919 \u24220 \u39640 \u32423 \u23448 \u21592 \u12289 \u36328 \u22269 \u20225 \u19994 \u12289 \u34892 \u19994 \u24040 \u22836 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u26696 \u20214 \u26159 \u21542 \u28041 \u21450 \u25919 \u24220 \u25110 \u25919 \u31574 \u38382 \u39064 ", \
+            weightLegal: 8, \
+            weightPR: 10,\
+            criteria: [\
+                "\uc0\u32431 \u31169 \u20154 \u32416 \u32439 \u65292 \u26080 \u25919 \u31574 \u20851 \u32852 ",\
+                "\uc0\u28041 \u21450 \u22320 \u26041 \u25919 \u31574 \u25191 \u34892 ",\
+                "\uc0\u28041 \u21450 \u20840 \u22269 \u24615 \u25919 \u31574 \u20294 \u26080 \u30417 \u31649 \u21387 \u21147 ",\
+                "\uc0\u21487 \u33021 \u24341 \u21457 \u25919 \u31574 \u35843 \u25972 \u25110 \u30417 \u31649 \u20171 \u20837 ",\
+                "\uc0\u26696 \u20214 \u30452 \u25509 \u25361 \u25112 \u29616 \u34892 \u27861 \u24459 /\u25919 \u31574 \u35268 \u23450 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u23458 \u25143 \u35785 \u27714 ", \
+            weightLegal: 5, \
+            weightPR: 12,\
+            criteria: [\
+                "\uc0\u20070 \u38754 \u36947 \u27465 ",\
+                "\uc0\u20572 \u27490 \u20405 \u26435 \u34892 \u20026 ",\
+                "\uc0\u21697 \u29260 \u20462 \u22797 ",\
+                "\uc0\u32463 \u27982 \u36180 \u20607 ",\
+                "\uc0\u36164 \u36136 \u24674 \u22797 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u32463 \u27982 \u25439 \u22833 ", \
+            weightLegal: 8, \
+            weightPR: 3,\
+            criteria: [\
+                "\uc0\u26080 \u27861 \u20272 \u31639 ",\
+                "\uc0\u27169 \u22411 \u35823 \u24046 ",\
+                "\uc0\u21512 \u29702 \u20272 \u31639 ",\
+                "\uc0\u20250 \u35745 \u24072 \u20107 \u21153 \u25152 \u19987 \u39033 \u23457 \u35745 ",\
+                "\uc0\u32463 \u23457 \u35745 \u30340 \u36130 \u21153 \u25253 \u21578 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u26696 \u20214 \u22797 \u26434 \u24615 ", \
+            weightLegal: 15, \
+            weightPR: 5,\
+            criteria: [\
+                "\uc0\u31616 \u21333 \u26126 \u30830 ",\
+                "\uc0\u28041 \u21450 \u21333 \u19968 \u27861 \u24459 \u38382 \u39064 ",\
+                "\uc0\u28041 \u21450 \u22810 \u20010 \u27861 \u24459 \u39046 \u22495 ",\
+                "\uc0\u28041 \u21450 \u36328 \u21306 \u22495 \u25110 \u36328 \u22269 \u27861 \u24459 \u38382 \u39064 ",\
+                "\uc0\u28041 \u21450 \u37325 \u22823 \u27861 \u24459 \u20105 \u35758 \u25110 \u25919 \u31574 \u35843 \u25972 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u30417 \u31649 \u39118 \u38505 ", \
+            weightLegal: 10, \
+            weightPR: 5,\
+            criteria: [\
+                "\uc0\u26080 \u39118 \u38505 ",\
+                "\uc0\u38480 \u26399 \u25972 \u25913 ",\
+                "\uc0\u35686 \u31034 \u20989 ",\
+                "\uc0\u30417 \u31649 \u32422 \u35848 ",\
+                "\uc0\u31435 \u26696 \u35843 \u26597 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u20010 \u20154 \u22768 \u35465 \u39118 \u38505 ", \
+            weightLegal: 5, \
+            weightPR: 25,\
+            criteria: [\
+                "\uc0\u26080 \u20010 \u20154 \u22768 \u35465 \u24433 \u21709 ",\
+                "\uc0\u22312 \u23567 \u33539 \u22260 \u19987 \u19994 \u22280 \u23618 \u20256 \u25773 ",\
+                "\uc0\u34892 \u19994 \u20869 \u35752 \u35770 \u65292 \u24433 \u21709 \u19987 \u19994 \u20449 \u29992 ",\
+                "\uc0\u23186 \u20307 \u20851 \u27880 \u65292 \u25439 \u23475 \u31038 \u20250 \u22768 \u35465 ",\
+                "\uc0\u20005 \u37325 \u25439 \u23475 \u65292 \u23548 \u33268 \u32844 \u19994 \u29983 \u28079 \u32456 \u27490 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u20225 \u19994 \u22768 \u35465 \u39118 \u38505 ", \
+            weightLegal: 5, \
+            weightPR: 30,\
+            criteria: [\
+                "\uc0\u26080 \u20225 \u19994 \u22768 \u35465 \u24433 \u21709 ",\
+                "\uc0\u20165 \u20869 \u37096 \u21592 \u24037 \u30693 \u26195 ",\
+                "\uc0\u34892 \u19994 \u20869 \u20256 \u25773 \u65292 \u24433 \u21709 \u21830 \u19994 \u21512 \u20316 ",\
+                "\uc0\u20844 \u20247 \u23186 \u20307 \u20851 \u27880 \u65292 \u20225 \u19994 \u24418 \u35937 \u21463 \u25439 ",\
+                "\uc0\u21697 \u29260 \u24418 \u35937 \u20005 \u37325 \u21463 \u25439 \u65292 \u23548 \u33268 \u24066 \u22330 \u20449 \u20219 \u21361 \u26426 "\
+            ]\
+        \},\
+        \{ \
+            name: "\uc0\u26696 \u20214 \u26631 \u30340 ", \
+            weightLegal: 10, \
+            weightPR: 10,\
+            criteria: [\
+                "\uc0\u8804 50\u19975 ",\
+                "50\uc0\u19975 -100\u19975 ",\
+                "100\uc0\u19975 -300\u19975 ",\
+                "300\uc0\u19975 -500\u19975 ",\
+                ">500\uc0\u19975 "\
+            ]\
+        \}\
+    ]\
+\};\
+\
+function createScoreSelector(dimension, index) \{\
+    const container = document.createElement('div');\
+    container.className = 'score-selector';\
+    \
+    dimension.criteria.forEach((criterion, i) => \{\
+        const score = i + 1;\
+        const option = document.createElement('div');\
+        option.className = 'score-option';\
+        option.innerHTML = `\
+            <input type="radio" name="score-$\{index\}" value="$\{score\}" \
+                id="score-$\{index\}-$\{score\}" $\{score === 1 ? 'checked' : ''\}>\
+            <label for="score-$\{index\}-$\{score\}">\
+                <span class="score-value">$\{score\}\uc0\u20998 </span>\
+                <span class="score-desc">$\{criterion\}</span>\
+            </label>\
+        `;\
+        container.appendChild(option);\
+    \});\
+\
+    return container;\
+\}\
+\
+function loadDimensions() \{\
+    const dimensions = model.dimensions;\
+    let html = `<div class="dimensions-grid">`;\
+    \
+    dimensions.forEach((dim, index) => \{\
+        html += `\
+            <div class="dimension-card">\
+                <div class="dimension-header">\
+                    <h3>$\{dim.name\}</h3>\
+                </div>\
+                <div class="score-container" id="score-container-$\{index\}"></div>\
+            </div>`;\
+    \});\
+\
+    html += `</div>`;\
+\
+    document.getElementById("dimensions-content").innerHTML = html;\
+\
+    dimensions.forEach((dim, index) => \{\
+        const container = document.getElementById(`score-container-$\{index\}`);\
+        container.appendChild(createScoreSelector(dim, index));\
+    \});\
+\
+    const inputs = document.querySelectorAll('input[type="radio"]');\
+    inputs.forEach(input => \{\
+        input.addEventListener('change', () => \{\
+            const calculateBtn = document.getElementById('calculate-btn');\
+            calculateBtn.disabled = false;\
+            calculateBtn.textContent = '\uc0\u29983 \u25104 \u35780 \u20272 \u32467 \u26524 ';\
+        \});\
+    \});\
+\}\
+\
+function calculateScore() \{\
+    const dimensions = model.dimensions;\
+    let judicialScore = 0, prScore = 0;\
+\
+    dimensions.forEach((dim, index) => \{\
+        const selectedScore = parseInt(\
+            document.querySelector(`input[name="score-$\{index\}"]:checked`).value\
+        );\
+        judicialScore += selectedScore * dim.weightLegal / 100;\
+        prScore += selectedScore * dim.weightPR / 100;\
+    \});\
+\
+    const diff = judicialScore - prScore;\
+    displayResult(judicialScore, prScore, diff);\
+\}\
+\
+function displayResult(judicial, pr, diff) \{\
+    let conclusion, icon, decisionClass;\
+\
+    if (diff >= 1.5) \{\
+        conclusion = "\uc0\u24378 \u28872 \u24314 \u35758 \u20248 \u20808 \u37319 \u21462 \u21496 \u27861 \u36884 \u24452 \u35299 \u20915 ";\
+        icon = "\uc0\u9878 \u65039 ";\
+        decisionClass = "judicial";\
+    \} else if (diff >= 0.5) \{\
+        conclusion = "\uc0\u24314 \u35758 \u20197 \u21496 \u27861 \u36884 \u24452 \u20026 \u20027 \u65292 \u36741 \u20197 \u20844 \u20851 \u25163 \u27573 ";\
+        icon = "\uc0\u55357 \u56540 ";\
+        decisionClass = "judicial";\
+    \} else if (diff >= -0.5) \{\
+        conclusion = "\uc0\u24314 \u35758 \u37319 \u21462 \u21452 \u36712 \u24182 \u34892 \u31574 \u30053 \u65292 \u21496 \u27861 \u19982 \u20844 \u20851 \u24182 \u37325 ";\
+        icon = "\uc0\u55357 \u56580 ";\
+        decisionClass = "dual";\
+    \} else \{\
+        conclusion = "\uc0\u24314 \u35758 \u20248 \u20808 \u37319 \u21462 \u20844 \u20851 \u36884 \u24452 \u35299 \u20915 ";\
+        icon = "\uc0\u55358 \u56605 ";\
+        decisionClass = "pr";\
+    \}\
+\
+    const conclusionDiv = document.getElementById("conclusion");\
+    conclusionDiv.innerHTML = `\
+        <h3>$\{icon\} \uc0\u35780 \u20272 \u32467 \u26524 \u20998 \u26512 </h3>\
+        <div class="conclusion-grid">\
+            <div class="score-card">\
+                <div class="label">\uc0\u21496 \u27861 \u35780 \u20998 </div>\
+                <div class="value">$\{judicial.toFixed(2)\}</div>\
+            </div>\
+            <div class="score-card">\
+                <div class="label">\uc0\u20844 \u20851 \u35780 \u20998 </div>\
+                <div class="value">$\{pr.toFixed(2)\}</div>\
+            </div>\
+            <div class="score-card">\
+                <div class="label">\uc0\u31574 \u30053 \u24046 \u20540 </div>\
+                <div class="value">$\{diff.toFixed(2)\}</div>\
+            </div>\
+        </div>\
+        <div class="decision $\{decisionClass\}">\
+            <strong>\uc0\u20915 \u31574 \u24314 \u35758 \u65306 </strong>$\{conclusion\}\
+        </div>\
+    `;\
+    conclusionDiv.className = 'conclusion show';\
+\}\
+\
+loadDimensions();\
+\
+const calculateBtn = document.getElementById('calculate-btn');\
+calculateBtn.addEventListener('click', calculateScore);}
